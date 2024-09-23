eeec266423c2899ba00bb8469768cc6276b83920
Matheus Roberto da Silva <77095461+vunkie@users.noreply.github.com>
2024-09-23 10:12:40 -0300

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -23,7 +23,7 @@ const Login = () => {
     formState: { errors, isValid },
   } = useForm<IFormLogin>({
     resolver: yupResolver(schema),
-    mode: "onBlur",
+    mode: "onChange",
     defaultValues,
     reValidateMode: "onChange",
   });
@@ -49,7 +49,7 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title="Entrar" disabled={!isValid}/>
         </Column>
       </LoginContainer>
     </Container>
